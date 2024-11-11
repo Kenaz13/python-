86a3b359dd0082c66e7919a8a6e5a4fb0ea53c02
Kenaz13 <kenazmathukutty13@gmail.com>
2024-11-11 22:26:00 +0530

    python program

---
+++ Kenaz.py
@@ -0,0 +1,10 @@
+'''
+Python progrm to find version of Python
+Author: Kenaz mathukutty
+Created:11/11/2024
+'''
+import sys
+print("python version")
+print(sys.version)
+print("version info")
+print(sys.version_info)
